586380fe582459c2bcdf3be9733c8112e2e49d4b
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-03-09 22:09:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let server = require('./qr'),
-let code = require('./pair');
+    code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/qr', server);
 app.use('/code', code);
