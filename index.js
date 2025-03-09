bb6ad500b3e636842d154cadd42c23811d9eb4d7
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-03-09 23:13:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let server = require('./qr'),
-    code = require('./pair');
+    
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/qr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
+
+
 app.use('/',async (req, res, next) => {
 res.sendFile(__path + '/main.html')
 })
